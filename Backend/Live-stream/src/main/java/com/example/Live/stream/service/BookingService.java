02fe584e99d6c1ba6f728515e02bb1fddeb67e8b
Dayastan <dayastant@gmail.com>
2026-03-09 18:22:07 +0530

    dd BookingService

---
+++ Backend/Live-stream/src/main/java/com/example/Live/stream/service/BookingService.java
@@ -0,0 +1,17 @@
+package com.example.Live.stream.service;
+
+import com.example.Live.stream.service.dto.BookingDTO;
+import java.util.List;
+
+public interface BookingService {
+
+    BookingDTO createBooking(BookingDTO dto);
+
+    BookingDTO confirmBooking(String id);
+
+    BookingDTO updatePaymentAndPrice(String id, BookingDTO dto);
+
+    List<BookingDTO> getAllBookings();
+
+    BookingDTO getBooking(String id);
+}
